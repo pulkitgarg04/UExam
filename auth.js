6433fc0ead583b5d4e5ab7f7b01b28910adf1832
Pulkit Garg <pulkitgargbnl@gmail.com>
2024-03-18 15:32:52 +0530

    fix api

--- auth.js
+++ auth.js
@@ -1,14 +1,14 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth2').Strategy;
 
-CLIENTID: "322533522504-fvjeefbu8nisf9pk72m81r1sr7qjfskh.apps.googleusercontent.com";
-CLIENTSECRET: "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";
+const CLIENTID = "322533522504-fvjeefbu8nisf9pk72m81r1sr7qjfskh.apps.googleusercontent.com";
+const CLIENTSECRET = "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";
 
 passport.use(new GoogleStrategy({
-    // clientID: CLIENTID,
-    // clientSecret: CLIENTSECRET,
-    clientID: process.env.CLIENTID,
-    clientSecret: process.env.CLIENTSECRET,
+    clientID: CLIENTID,
+    clientSecret: CLIENTSECRET,
+    // clientID: process.env.CLIENTID,
+    // clientSecret: process.env.CLIENTSECRET,
     // callbackURL: "http://localhost:8080/auth/google/callback",
     callbackURL: "/auth/google/callback",
     passReqToCallback: true
